bd39ceca6ad89bc5b08e7c61a50482818e623259
Koray <koray_4404@hotmail.com>
2023-04-07 22:55:09 +0100

    Test Automation day16. GoogleStepDefinitions.java googleSearch.feature & GoogleSearchPage.java updated

--- src/test/java/com/cydeo/pages/GoogleSearchPage.java
+++ src/test/java/com/cydeo/pages/GoogleSearchPage.java
@@ -1,22 +1,27 @@
 package com.cydeo.pages;
 
-import com.cydeo.utilities.Driver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+import com.cydeo.utilities.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.*;
 
 public class GoogleSearchPage {
-    //Create constructor
-    //initialize the driver instance and this class' instance using PageFactory.initElements
+
+    //create constructor
+    // initialize the driver instance and this class' instance using PageFactory.initElements
     public GoogleSearchPage(){
         PageFactory.initElements(Driver.getDriver(), this);
     }
 
     //We can start locating web elements using @FindBy annotation
-
     @FindBy(name = "q")
     public WebElement searchBox;
 
+//    public WebElement capitalElement(String capital){
+//        return Driver.getDriver().findElement(By.linkText(capital));
+//    }
 
+    public String getCapital(){
+        return Driver.getDriver().findElement(By.xpath("//a[@class='FLP8od']")).getText();
+    }
 
 }
--- src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/GoogleStepDefinitions.java
@@ -1,30 +1,54 @@
 package com.cydeo.step_definitions;
 
-import com.cydeo.pages.GoogleSearchPage;
-import com.cydeo.utilities.Driver;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.junit.Assert;
-import org.openqa.selenium.Keys;
+import com.cydeo.pages.*;
+import com.cydeo.utilities.*;
+import io.cucumber.java.en.*;
+
+import org.junit.*;
+import org.openqa.selenium.*;
+
 
 public class GoogleStepDefinitions {
 
     GoogleSearchPage googleSearchPage = new GoogleSearchPage();
 
-
     @When("user types apple and clicks enter")
-    public void user_types_apple_and_clicks_enter() {
-        googleSearchPage.searchBox.sendKeys("apple"+ Keys.ENTER);
+    public void user_types_and_clicks_enter2() {
+
+        googleSearchPage.searchBox.sendKeys("apple" + Keys.ENTER);
+
     }
+
+    @When("user types {string} and clicks enter")
+    public void user_types_and_clicks_enter(String searchKeyword) {
+
+//        WebDriverWait wait = new WebDriverWait(Driver.getDriver(),10);
+//        wait.until(ExpectedConditions.visibilityOf(googleSearchPage.searchBox));
+        googleSearchPage.searchBox.sendKeys(searchKeyword + Keys.ENTER);
+
+    }
+
+    @Then("user sees {string} in the google title")
+    public void user_sees_in_the_google_title(String string) {
+
+        String expectedTitle = string+" - Google Search";
+        String actualTitle = Driver.getDriver().getTitle();
+
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+
+    }
+
     @Then("user sees apple in the google title")
     public void user_sees_apple_in_the_google_title() {
 
         String expectedTitle = "apple - Google Search";
         String actualTitle = Driver.getDriver().getTitle();
 
-        //Junit assertion accepts first arg as expected, second arg as actual (below)
-        Assert.assertEquals("Title is not as expected" ,expectedTitle,actualTitle);
-       // Assert.assertEquals(actualTitle.equals(expectedTitle));
+        //Junit assertion accepts first arg as expected, second arg as actual
+        Assert.assertEquals("Title is not as expected!",expectedTitle, actualTitle);
+
+        //Assert.assertTrue(actualTitle.equals(expectedTitle));
 
     }
 
@@ -40,8 +64,16 @@ public class GoogleStepDefinitions {
         String expectedTitle = "Google";
         String actualTitle = Driver.getDriver().getTitle();
 
-        Assert.assertEquals(expectedTitle,actualTitle);
+        Assert.assertEquals(expectedTitle, actualTitle);
+
+
 
     }
 
+
+    @Then("User should see {string} in the result")
+    public void userShouldSeeInTheResult(String expectedCapital) {
+        String actualValue = googleSearchPage.getCapital();
+        Assert.assertEquals(expectedCapital,actualValue);
+    }
 }
